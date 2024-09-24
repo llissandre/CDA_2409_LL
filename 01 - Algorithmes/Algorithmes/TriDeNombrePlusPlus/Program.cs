72543e2e2f5b40b12e468c4b5531d686dcd86eaa
llissandreCRM <118750600+llissandre@users.noreply.github.com>
2024-09-24 12:04:37 +0200

    Premier Commit

---
+++ 01 - Algorithmes/Algorithmes/TriDeNombrePlusPlus/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TriDeNombrePlusPlus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
