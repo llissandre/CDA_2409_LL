d16bb295e41b848d16477d5ffae17e0f593f256e
llissandreCRM <118750600+llissandre@users.noreply.github.com>
2024-09-25 16:54:31 +0200

    NombrePremier en cours

---
+++ 01 - Algorithmes/Algorithmes/NombrePremier/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int demanderUnNombre(string Phrase)
+{
+    int nb;
+    string saisie;
+    bool vrai;
+
+    Console.Write(Phrase);
+
+    do
+    {
+        Console.Write("Entrez un entier : ");
+        saisie = Console.ReadLine();
+        vrai = int.TryParse(saisie, out nb);
+    } while (!vrai);
+
+    return nb;
+}
+
+long nombre;
+int nbPremier;
+
+nombre = demanderUnNombre("");
+nbPremier = 0;
+
+for (int i = 2; i <= nombre / 2; i++)
+{
+    if (nombre % i == 0)
+    {
+        nbPremier ++;
+    }
+}
+
+if (nbPremier >= 1)
+{
+    Console.WriteLine("Il ne s'agit pas d'un nombre premier");
+}
+else
+{
+    Console.WriteLine("Il s'agit d'un nombre premier");
+}
+
+//Console.ReadLine();
