1b46f68bf0f28073b3dd0ef4e6a18bb71858a559
llissandreCRM <118750600+llissandre@users.noreply.github.com>
2024-09-26 08:47:36 +0200

    Début JeuDeLaFourchette

---
+++ 01 - Algorithmes/Algorithmes/JeuDeLaFourchette/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int demanderUnNombreEntiers(string Phrase)
+{
+    int nb;
+    string saisie;
+    bool vrai;
+
+    Console.WriteLine(Phrase);
+
+    do
+    {
+        Console.WriteLine("Entrer un nombre");
+        saisie = Console.ReadLine();
+        vrai=int.TryParse(saisie, out nb);
+    }
+    while (!vrai);
+
+    return nb;
+}
+
+Random rand;
+int nombre;
+
+
+
