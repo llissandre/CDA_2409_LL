61f342f5e656e6ccc21f26cd20dc1914e567c6fb
llissandreCRM <118750600+llissandre@users.noreply.github.com>
2024-10-07 08:19:10 +0200

    Début de BarnabeFaitSesCourses

---
+++ 01 - Algorithmes/Algorithmes/BarnabeFaitSesCourses/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int sommeADepenser(string phrase)
+{
+    int somme;
+    string saisie;
+    bool vrai;
+
+    Console.WriteLine(Phrase);
+
+    do
+    {
+        Console.Write("Entrez la somme à dépenser : ");
+        saisie = Console.ReadLine();
+        vrai=int.TryParse(saisie, out somme);
+    }
+    while (!vrai);
+
+    return somme;
+}
+
+int store;
+int somme;
+
+store = 0;
+somme = sommeADepenser("");
+
+do
+{
+
+} while (somme>1);
