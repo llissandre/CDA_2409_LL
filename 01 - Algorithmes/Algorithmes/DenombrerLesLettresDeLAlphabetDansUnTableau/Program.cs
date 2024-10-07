10867351cb645d2f76bef223c5a883e99b3179bf
llissandreCRM <118750600+llissandre@users.noreply.github.com>
2024-10-07 15:44:23 +0200

    DenombrerLesLettresDeLAlphabetDansUnTableau en cours

--- 01 - Algorithmes/Algorithmes/DenombrerLesLettresDeLAlphabetDansUnTableau/Program.cs
+++ 01 - Algorithmes/Algorithmes/DenombrerLesLettresDeLAlphabetDansUnTableau/Program.cs
@@ -1,3 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+string entrerDuTexte(string phrase)
+{
+    Console.WriteLine("Taper du texte : ");
+    return Console.ReadLine();
+}
+
+string text;
+int count;
+int countLetter;
+string[] monTableau;
+
+text = entrerDuTexte("");
+count = 0;
+countLetter = 0;
+
+monTableau = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+foreach (char c in text)
+{
+    count++;
+}
+
+Console.WriteLine($"Nombre de caractères du texte : {count}");
+
+if (count < 10)
+{
+    Console.WriteLine("Le texte doit contenir au moins 120 caractères !");
+}
+else
+{
+    for (int i = 0; i < monTableau.Length; i++)
+    {
+        for (int j = 0; j < text.Length; j++)
+        {
+            if (monTableau[i].CompareTo(text[j])
+            {
+
+            }
+            Console.WriteLine(text[0]);
+            Console.WriteLine(monTableau[0]);
+        }
+    }
+}
