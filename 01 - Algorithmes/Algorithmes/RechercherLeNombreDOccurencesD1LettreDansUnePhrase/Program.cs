0e401b05f84378452d0003c9470db81e62fa022b
llissandreCRM <118750600+llissandre@users.noreply.github.com>
2024-10-07 12:19:02 +0200

    Fin de RechercherLeNombreDOccurencesD1LettreDansUnePhrase

--- 01 - Algorithmes/Algorithmes/RechercherLeNombreDOccurencesD1LettreDansUnePhrase/Program.cs
+++ 01 - Algorithmes/Algorithmes/RechercherLeNombreDOccurencesD1LettreDansUnePhrase/Program.cs
@@ -1,8 +1,59 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string entrerUnePhrase(string text)
+string entrerUnePhrase(string phrase)
 {
-    Console.WriteLine("Entrer une phrase");
+    Console.Write("Entrer une phrase : ");
+    return Console.ReadLine();
+}
 
-    return text;
+char entrerUnCaractere(char caractere)
+{
+    Console.Write("Entrer un caractère : ");
+    return Console.ReadKey().KeyChar;
+}
+
+string phrase;
+char caractere;
+int compteur;
+int compteurP;
+
+phrase = entrerUnePhrase("");
+compteur = 0;
+compteurP = 0;
+
+foreach (char c in phrase)
+{
+    compteur++;
+
+    if (c == '.')
+    {
+        compteurP++;
+    }
+}
+
+if (phrase.Length == 0 || compteur == compteurP)
+{
+    Console.WriteLine("LA CHAINE EST VIDE");
+}
+else
+{
+    compteur = 0;
+    caractere = entrerUnCaractere(' ');
+
+    foreach (char c in phrase)
+    {
+        if (c == caractere)
+        {
+            compteur++;
+        }
+    }
+
+    if (compteur == 0)
+    {
+        Console.WriteLine($"{Environment.NewLine}La lettre {caractere} n'est pas présente");
+    }
+    else
+    {
+        Console.WriteLine($"{Environment.NewLine}La lettre {caractere} est présente {compteur} fois");
+    }
 }
