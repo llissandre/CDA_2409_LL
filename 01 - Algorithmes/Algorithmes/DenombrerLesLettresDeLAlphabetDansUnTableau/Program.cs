28d5ab780477fb52cc4f03394147a28cc82eceaa
llissandreCRM <118750600+llissandre@users.noreply.github.com>
2024-10-08 09:12:41 +0200

    Fin de DenombrerLesLettresDeLAlphabetDansUnTableau

--- 01 - Algorithmes/Algorithmes/DenombrerLesLettresDeLAlphabetDansUnTableau/Program.cs
+++ 01 - Algorithmes/Algorithmes/DenombrerLesLettresDeLAlphabetDansUnTableau/Program.cs
@@ -1,22 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using System.ComponentModel;
+
 string entrerDuTexte(string phrase)
 {
     Console.WriteLine("Taper du texte : ");
-    return Console.ReadLine();
+    phrase = Console.ReadLine();
+    phrase = phrase.ToLower();
+    return phrase;
 }
 
 string text;
 int count;
 int countLetter;
-string[] monTableau;
+char[] monTableau;
 
 text = entrerDuTexte("");
 count = 0;
 countLetter = 0;
 
-monTableau = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+monTableau = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 
 foreach (char c in text)
@@ -24,24 +28,25 @@ foreach (char c in text)
     count++;
 }
 
-Console.WriteLine($"Nombre de caractères du texte : {count}");
+Console.WriteLine($"Nombre de caractères du texte : {count}{Environment.NewLine}");
 
 if (count < 10)
 {
-    Console.WriteLine("Le texte doit contenir au moins 120 caractères !");
+    Console.WriteLine($"Le texte doit contenir au moins 120 caractères !{Environment.NewLine}");
 }
 else
 {
     for (int i = 0; i < monTableau.Length; i++)
     {
-        for (int j = 0; j < text.Length; j++)
+        foreach (char c in text)
         {
-            if (monTableau[i].CompareTo(text[j])
+            //if (c == monTableau[i])
+            if (c.Equals(monTableau[i]))
             {
-
+                countLetter++;
             }
-            Console.WriteLine(text[0]);
-            Console.WriteLine(monTableau[0]);
         }
+        Console.WriteLine($"La lettre {monTableau[i]} est présente {countLetter} fois !");
+        countLetter = 0;
     }
 }
