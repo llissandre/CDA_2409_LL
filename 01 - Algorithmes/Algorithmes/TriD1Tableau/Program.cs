81b79be2aa8ec06353ab4100e61f0558dac5d33c
llissandreCRM <118750600+llissandre@users.noreply.github.com>
2024-10-08 16:56:50 +0200

    TriD1Tableau en cours

--- 01 - Algorithmes/Algorithmes/TriD1Tableau/Program.cs
+++ 01 - Algorithmes/Algorithmes/TriD1Tableau/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace TriD1Tableau
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] tableau;
+int j;
+int temp;
+int temp2;
+
+tableau = [128, 64, 8, 512, 16, 32, 256];
+
+//for (int i = 0; i < 1; i++)
+//{
+temp = tableau[0];
+Console.WriteLine(temp);
+
+for (int i2 = 1; i2 < tableau.Length; i2++)
 {
-    internal class Program
+    if (tableau[i2] < temp)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+
     }
+}
+//}
+
+foreach (int i in tableau)
+{
+    Console.Write(i);
 }
