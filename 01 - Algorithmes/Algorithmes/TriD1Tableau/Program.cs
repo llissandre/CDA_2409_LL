0153abb93fd35a492ac7418722d76940ae405e32
llissandreCRM <118750600+llissandre@users.noreply.github.com>
2024-10-09 09:03:52 +0200

    Première inversion dans TriD1Tableau

--- 01 - Algorithmes/Algorithmes/TriD1Tableau/Program.cs
+++ 01 - Algorithmes/Algorithmes/TriD1Tableau/Program.cs
@@ -1,25 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 int[] tableau;
-int j;
 int temp;
 int temp2;
+int i1=0;
+int i2=0;
 
 tableau = [128, 64, 8, 512, 16, 32, 256];
 
 //for (int i = 0; i < 1; i++)
 //{
-temp = tableau[0];
+temp = tableau[i1];
+temp2 = tableau[i1];
 Console.WriteLine(temp);
 
-for (int i2 = 1; i2 < tableau.Length; i2++)
+for (int j = 1; j < tableau.Length; j++)
 {
-    if (tableau[i2] < temp)
+    if (tableau[j] < temp && tableau[j] < temp2)
     {
-
+        temp2 = tableau[j];
+        i2 = j;
     }
 }
 //}
+
+tableau[i2] = temp;
+tableau[i1] = temp2;
+
 
 foreach (int i in tableau)
 {
