141d20e030cb74b242df43abe6d98c39371a42c3
llissandreCRM <118750600+llissandre@users.noreply.github.com>
2024-10-09 12:53:37 +0200

    Début de Palindrome

---
+++ 01 - Algorithmes/Algorithmes/Palindrome/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string entrerUneChaineDeCaractere(string Phrase)
+{
+    Console.Write(Phrase);
+
+    Console.Write("Entrez une chaine de caractères : ");
+    return Console.ReadLine();
+}
+
