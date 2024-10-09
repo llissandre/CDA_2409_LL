22ca507541bf463eead6de481c66b25b56df3f00
llissandreCRM <118750600+llissandre@users.noreply.github.com>
2024-10-09 14:18:13 +0200

    Début de Evaluation des connaissances en algorithme en C#

---
+++ 01 - Algorithmes/Algorithmes/Evaluation des connaissances en algorithme en C#/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Solutionnez la problématique suivante dans une application en mode « Console » dans un des langages suivants : 
+//C#.
+
+//Au CRM, chaque stagiaire et chaque membre du personnel dispose d’une carte à son nom. 
+//Pour régler les consommations avec la carte, il est nécessaire de l’alimenter en €. (Principe de la « carte prépayée »). 
+//Pour régler un repas au restaurant du CRM, la carte est vérifiée :
+//-Les données de la carte correspondent-elle à une personne enregistrée ?
+//-   Y’a-t-il suffisamment de fonds disponibles ?
+//Si les contrôles sont validés, le prix du repas est soustrait de la somme disponible sur la carte.
+
+//Votre travail consiste à développer l’algorithme de validation de la carte.
+
+//Pour simuler le fonctionnement, l’algorithme sera programmé dans une application en mode ‘Console’. 
+//-   Le tableau ‘utilisateurs’ contiendra 5 utilisateurs.
+//-   Le prix du repas sera fixé à 4 €
+//-   Il n’est pas possible d’être « à découvert »
+
--- 01 - Algorithmes/Algorithmes/Palindrome/Program.cs
+++ 01 - Algorithmes/Algorithmes/Palindrome/Program.cs
@@ -3,9 +3,58 @@
 
 string entrerUneChaineDeCaractere(string Phrase)
 {
-    Console.Write(Phrase);
-
     Console.Write("Entrez une chaine de caractères : ");
     return Console.ReadLine();
 }
 
+string phrase = entrerUneChaineDeCaractere("");
+char caractere;
+int compteur = 0;
+int compteurP = 0;
+
+
+do
+{
+    foreach (char c in phrase)
+    {
+        compteur++;
+
+        if (c == '.')
+        {
+            compteurP++;
+        }
+    }
+
+    Console.WriteLine("LA CHAINE EST VIDE");
+    phrase = entrerUneChaineDeCaractere("");
+}
+while (compteur == compteurP);
+
+if (phrase.Length == 0 || compteur == compteurP)
+{
+    Console.WriteLine("LA CHAINE EST VIDE");
+}
+
+
+else
+{
+    compteur = 0;
+    phrase = entrerUneChaineDeCaractere("");
+
+    foreach (char c in phrase)
+    {
+        if (c == caractere)
+        {
+            compteur++;
+        }
+    }
+
+    if (compteur == 0)
+    {
+        Console.WriteLine($"{Environment.NewLine}La lettre {caractere} n'est pas présente");
+    }
+    else
+    {
+        Console.WriteLine($"{Environment.NewLine}La lettre {caractere} est présente {compteur} fois");
+    }
+}
