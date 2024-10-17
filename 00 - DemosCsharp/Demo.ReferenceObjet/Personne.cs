ee1c8cac11669d518816cad631025c6d39a74fb8
llissandreCRM <118750600+llissandre@users.noreply.github.com>
2024-10-17 11:29:40 +0200

    Ajout de 00-DemosCsharp

---
+++ 00 - DemosCsharp/Demo.ReferenceObjet/Personne.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.ReferenceObjet
+{
+    internal class Personne
+    {
+        public int age;
+
+        public Personne(int _age)
+        {
+            this.age = _age;
+        }
+    }
+}
---
+++ 00 - DemosCsharp/Demo.References/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Demo.References
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // étudiez le code suivant et tentez de deviner la sortie du programme
+
+            Console.WriteLine("Passage par valeur et/ou par référence");
+
+            int A = 1;
+
+            Toto(ref A);
+
+            Console.WriteLine("A: " + A);
+
+        }
+
+        static void Toto(ref int B)
+        {
+            B = B + 1;
+
+            Console.WriteLine("B: " + B);
+        }
+    }
+}
