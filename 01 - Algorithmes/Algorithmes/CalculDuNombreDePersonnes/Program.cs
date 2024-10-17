b3e4caa0f2e447df77a33e2983ca7808c2a4f321
llissandreCRM <118750600+llissandre@users.noreply.github.com>
2024-10-17 09:22:47 +0200

    Fin de la partie exercice 6.3.1 : Calculer le nombre de jeunes

--- 01 - Algorithmes/Algorithmes/CalculDuNombreDePersonnes/Program.cs
+++ 01 - Algorithmes/Algorithmes/CalculDuNombreDePersonnes/Program.cs
@@ -2,9 +2,55 @@
 {
     internal class Program
     {
+
+        static int age;
+        static int iterator;
+        static string[] monTableau = new string[20];
+        static bool vrai;
+
+
+        static string[] calculLeNbDeJeunes(string phrase)
+        {
+            iterator = 0;
+            do
+            {
+                do
+                {
+                    Console.Write("Saisir un nombre entier : ");
+                    vrai = int.TryParse(Console.ReadLine(), out age);
+                }
+                while (!vrai);
+
+                monTableau[iterator] = age.ToString();
+                iterator++;
+            }
+            while (iterator < 20);
+
+            return monTableau;
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] monTableauDAges = new string[20];
+
+            monTableauDAges = calculLeNbDeJeunes("");
+
+            iterator = 0;
+
+            foreach (string monTableauDAge in monTableauDAges)
+            {
+                age = int.Parse(monTableauDAge);
+
+                if (age < 20)
+                {
+                    iterator++;
+                }
+            }
+
+            Console.WriteLine();
+            Console.Write($"Nombre de jeunes : {iterator}.");
+            Console.WriteLine();
         }
     }
 }
