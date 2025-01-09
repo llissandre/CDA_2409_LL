b3f42334d48d1a1085617b4471a3b55b23a89276
llissandreCRM <118750600+llissandre@users.noreply.github.com>
2025-01-09 12:46:00 +0100

    Début de color generator

--- 02 - webfront/Exercices/Color-Generator/assets/JS/index.js
+++ 02 - webfront/Exercices/Color-Generator/assets/JS/index.js
@@ -1,5 +1,6 @@
 // Créer 3 variables pour stocker 3 chiffres aléatoires
-
+let r, g, b;
 // Donner au body la couleur de fond en rgb()
+document.body.style.backgroundColor = 'rgb(2, 2, 2)';
 
 // Montrer sur le body la couleur rgb()
