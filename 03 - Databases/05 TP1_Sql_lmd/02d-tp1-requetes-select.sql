2dcd009cf1199455af412d663465ad4c9eff517a
llissandreCRM <118750600+llissandre@users.noreply.github.com>
2025-01-14 12:59:53 +0100

    PART2 du TP1 BDD en cours

--- 03 - Databases/05 TP1_Sql_lmd/02d-tp1-requetes-select.sql
+++ 03 - Databases/05 TP1_Sql_lmd/02d-tp1-requetes-select.sql
@@ -16,7 +16,7 @@ FROM EMP;
 
 SELECT ename, job, empno, sal
 FROM EMP
-WHERE deptno = 20;
+WHERE deptno = 10;
 
 /* 2. Donner nom, job et salaire des employés de type MANAGER dont le salaire est supérieur à 2800 */
 SELECT ename, job, sal
@@ -26,7 +26,7 @@ WHERE job = 'MANAGER' AND sal > 2800;
 /* 3. Donner la liste des MANAGER n'appartenant pas au département 30 */
 SELECT ename
 FROM EMP
-WHERE job != 'MANAGER';
+WHERE job = 'MANAGER' AND deptno != 30;
 
 /* 4. Liste des employés de salaire compris entre 1200 et 1400 */
 SELECT ename
@@ -97,14 +97,18 @@ HAVING count(deptno) > 1
 ORDER BY deptno;
 
 /* 16. Liste des employés (Nom, département, salaire) de même emploi que JONES */
-SELECT ename, deptno, sal
-FROM EMP
-WHERE job = (SELECT job FROM EMP WHERE ename = 'JONES');
+SELECT ename, dname, sal
+FROM EMP e
+JOIN DEPT d
+ON e.deptno = d.deptno
+WHERE job = (SELECT job FROM EMP WHERE ename = 'JONES') AND ename != 'JONES';
 
 /* 17. Liste des employés (nom, salaire) dont le salaire est supérieur à la moyenne globale des salaires */
 SELECT ename, sal
 FROM EMP
 WHERE sal > (SELECT round(avg(sal)) FROM EMP);
+
+SELECT round(avg(sal)) FROM EMP;
 
 /* 18. Création d'une table PROJET avec comme colonnes numéro de projet (3 chiffres), nom de projet(5 caractères), budget. Entrez les valeurs suivantes: 
 	101, ALPHA, 96000
@@ -116,7 +120,10 @@ nom_proj CHAR(5) NOT NULL,
 bud_proj DECIMAL(8,2) NOT NULL
 );    
 
-ALTER TABLE PROJET AUTO_INCREMENT=100;
+DROP TABLE PROJET;
+TRUNCATE TABLE PROJET;
+
+ALTER TABLE PROJET AUTO_INCREMENT=101;
 
 INSERT INTO PROJET
 (nom_proj, bud_proj)
@@ -130,25 +137,40 @@ select * from PROJET;
 /* 19. Ajouter l'attribut numéro de projet à la table EMP et affecter tous les vendeurs du département 30 au projet 101, et les autres au projet 102 */
 ALTER TABLE EMP ADD num_proj SMALLINT;
 
-UPDATE EMP 
+UPDATE EMP
 SET num_proj = 101
 WHERE deptno = 30 AND job = 'SALESMAN';
 
-UPDATE EMP 
+UPDATE EMP
 SET num_proj = 102
-WHERE deptno != 30;
+WHERE NOT deptno = 30 AND job = 'SALESMAN';
+
+UPDATE EMP
+SET num_proj = 102
+WHERE deptno = 30 AND NOT job = 'SALESMAN';
+
+UPDATE EMP
+SET num_proj = 102
+WHERE NOT deptno = 30 AND NOT job = 'SALESMAN';
+
+SELECT *
+FROM EMP;
 
 /* 20. Créer une vue comportant tous les employés avec nom, job, nom de département et nom de projet */
 DROP VIEW V_employes;
 
-CREATE VIEW V_employes 
+CREATE VIEW V_employes
 AS SELECT ename, job, dname, nom_proj FROM EMP e 
 JOIN DEPT d ON e.deptno = d.deptno
 JOIN PROJET p ON e.num_proj = p.num_proj;
 
 /* 21. A l'aide de la vue créée précédemment, lister tous les employés avec nom, job, nom de département et nom de projet triés sur nom de département et nom de projet */
 SELECT *
-FROM V_employes;
+FROM V_employes
+ORDER BY dname, nom_proj;
+
+SELECT *
+FROM EMP;
 
 /* 22.Donner le nom du projet associé à chaque manager */
 SELECT ename, nom_proj
@@ -157,23 +179,95 @@ WHERE job = 'MANAGER';
 
 /* Deuxième partie */
 /* 1. Afficher la liste des managers des départements 20 et 30 */
-
+SELECT ename, job
+FROM EMP
+WHERE job = 'MANAGER' AND deptno IN(20, 30);
 
 /* 2. Afficher la liste des employés qui ne sont pas manager et qui ont été embauchés en 81 */
+SELECT ename
+FROM EMP
+WHERE job != 'MANAGER' AND YEAR(hiredate) = 1981;
+
 /* 3. Afficher la liste des employés ayant une commission */
+SELECT ename
+FROM EMP
+WHERE comm != 0 OR comm IS NOT NULL;
+
 /* 4. Afficher la liste des noms, numéros de département, jobs et date d'embauche triés par Numero de Département et JOB les derniers embauches d'abord. */
+SELECT ename, deptno, job, hiredate
+FROM EMP
+ORDER BY deptno, job, hiredate DESC;
+
 /* 5. Afficher la liste des employés travaillant à DALLAS */
+SELECT ename
+FROM EMP e
+JOIN DEPT d
+ON e.deptno = d.deptno
+WHERE loc = 'DALLAS';
+
 /* 6. Afficher les noms et dates d'embauche des employés embauchés avant leur manager, avec le nom et date d'embauche du manager. */
+SELECT e.ename AS 'employé', e.hiredate, m.ename AS 'manager', m.hiredate
+FROM EMP e
+JOIN EMP m
+ON e.mgr = m.empno
+WHERE e.hiredate < m.hiredate;
+
 /* 7. Lister les numéros des employés n'ayant pas de subordonné. */
+SELECT m.*, e.*
+FROM EMP e
+RIGHT JOIN EMP m
+ON e.mgr = m.empno;
+
+SELECT m.empno
+FROM EMP e
+RIGHT JOIN EMP m
+ON e.mgr = m.empno
+WHERE e.empno IS NULL;
+
 /* 8. Afficher les noms et dates d'embauche des employés embauchés avant BLAKE. */
+SELECT ename, hiredate
+FROM EMP
+WHERE hiredate < (SELECT hiredate FROM EMP WHERE ename = 'BLAKE');
+
 /* 9. Afficher les employés embauchés le même jour que FORD. */
+SELECT ename
+FROM EMP
+WHERE hiredate = (SELECT hiredate FROM EMP WHERE ename = 'FORD') AND ename != 'FORD';
+
 /* 10. Lister les employés ayant le même manager que CLARK. */
+SELECT ename
+FROM EMP
+WHERE mgr = (SELECT mgr FROM EMP WHERE ename = 'CLARK') AND ename != 'CLARK';
+
 /* 11. Lister les employés ayant même job et même manager que TURNER. */
+SELECT ename
+FROM EMP
+WHERE mgr = (SELECT mgr FROM EMP WHERE ename = 'TURNER') AND ename != 'TURNER' AND job = (SELECT job FROM EMP WHERE ename = 'TURNER');
+
 /* 12. Lister les employés du département RESEARCH embauchés le même jour que quelqu'un du département SALES. */
+SELECT ename, hiredate
+FROM EMP e
+JOIN DEPT d
+ON e.deptno = d.deptno
+WHERE dname = 'RESEARCH' AND hiredate IN(SELECT hiredate FROM EMP e JOIN DEPT d ON e.deptno = d.deptno WHERE dname = 'SALES');
+
 /* 13. Lister le nom des employés et également le nom du jour de la semaine correspondant à leur date d'embauche. */
+SELECT ename, DATE_FORMAT(hiredate, '%a') AS 'Jour de la semaine'
+FROM EMP;
+
 /* 14. Donner, pour chaque employé, le nombre de mois qui s'est écoulé entre leur date d'embauche et la date actuelle. */
+
+
 /* 15. Afficher la liste des employés ayant un M et un A dans leur nom. */
+SELECT ename
+FROM EMP
+WHERE ename LIKE '%M%' AND ename LIKE '%A%';
+
 /* 16. Afficher la liste des employés ayant deux 'A' dans leur nom. */
+SELECT ename
+FROM EMP
+WHERE ename LIKE '%M%' AND ename LIKE '%A%';
+
 /* 17. Afficher les employés embauchés avant tous les employés du département 10. */
 /* 18. Sélectionner le métier où le salaire moyen est le plus faible. */
 /* 19. Sélectionner le département ayant le plus d'employés. */
