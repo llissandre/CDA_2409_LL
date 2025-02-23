965a070ad5af7fb44e462d561b6317578704c464
llissandre <118750600+llissandre@users.noreply.github.com>
2025-02-23 19:29:48 +0100

    S1-1

---
+++ 99 - Documentations/S1 - Algorithmie/ExercicesAlgorithmie/Exercice02/E02Program.cs
@@ -0,0 +1,17 @@
+﻿//affiche sur la console : "Rentrer un nombre:"
+Console.WriteLine("Rentrez un nombre :");
+
+//récupère le nombre donné par l'utilisateur et le range dans
+//la variable N
+String N = Console.ReadLine();
+
+//parse la chaine de caractère "N" en un entier "Nombre"
+int Nombre = int.Parse(N);
+
+//affiche sur la console : "Le résultat du calcul est : " suivi
+//de l'opération de la variable multipliée trois fois par elle-
+//même
+Console.WriteLine("Le résultat du calcul est : " + Nombre * Nombre * Nombre);
+
+//permet de laisser la fenêtre de la console ouverte
+Console.ReadLine();
---
+++ 99 - Documentations/S1 - Algorithmie/ExercicesAlgorithmie/Exercice08TableauBis/E08TableauBisProgram.cs
@@ -0,0 +1,23 @@
+﻿
+int[] Tableau1 = { 4, 8, 7, 12 };
+int[] Tableau2 = { 3, 6 };
+int Produit = 0;
+int Schtroumpf = 0;
+int Iterateur2 = 0;
+
+for (int Iterateur1 = 0; Iterateur1 <= Tableau1.Length; Iterateur1++)
+{
+    if ((Iterateur1 == Tableau1.Length) && (Iterateur2 < Tableau2.Length - 1))
+    {
+        Iterateur2++;
+        Iterateur1 = 0;
+    }
+
+    if ((Iterateur1 < Tableau1.Length) && (Iterateur2 < Tableau2.Length))
+    {
+        Produit = Tableau1[Iterateur1] * Tableau2[Iterateur2];
+        Schtroumpf += Produit;
+    }
+}
+
+Console.WriteLine(Schtroumpf);
