8a801fb8cf4521886314057735cf09a97bdcfd3b
llissandre <118750600+llissandre@users.noreply.github.com>
2025-02-24 19:53:21 +0100

    Fin de l'exercice Manipuler les styles

--- 02 - webfront/JS-Intro/Exercices/Manipuler les styles/assets/index.js
+++ 02 - webfront/JS-Intro/Exercices/Manipuler les styles/assets/index.js
@@ -1,35 +1,54 @@
 const btnaugmenter = document.querySelector('#btnaugmenter');
 const btndiminuer = document.querySelector('#btndiminuer');
 const texte = document.querySelector('p');
-const px = document.querySelector('#px');
+const inputTaille = document.querySelector('input[type="number"]');
 const tailletextepardefaut = 16;
 const tailletextemaximale = 48;
 const tailletexteminimale = 8;
+let tailleActuelle = document.querySelector('#tailleActuelle');
 
 texte.style.fontSize = tailletextepardefaut + 'px';
 
 btnaugmenter.addEventListener('click', () => {
-    let tailledutexte = Number(document.querySelector('#px').textContent);
+    let tailledutexte = Number(tailleActuelle.textContent);
 
     if (tailledutexte < tailletextemaximale) {
-        document.querySelector('#px').textContent = tailledutexte + 1;
+        tailleActuelle.textContent = tailledutexte + 1;
         texte.style.fontSize = tailledutexte + 'px';
+        inputTaille.value = tailleActuelle.textContent;
     }
     else {
-        document.querySelector('#px').textContent = tailletextepardefaut;
+        tailleActuelle.textContent = tailletextepardefaut;
         texte.style.fontSize = tailletextepardefaut + 'px';
+        inputTaille.value = tailleActuelle.textContent;
     }
 });
 
 btndiminuer.addEventListener('click', () => {
-    let tailledutexte = Number(document.querySelector('#px').textContent);
+    let tailledutexte = Number(tailleActuelle.textContent);
 
     if (tailledutexte > tailletexteminimale) {
-        document.querySelector('#px').textContent = tailledutexte - 1;
+        tailleActuelle.textContent = tailledutexte - 1;
+        texte.style.fontSize = tailledutexte + 'px';
+        inputTaille.value = tailleActuelle.textContent;
+    }
+    else {
+        tailleActuelle.textContent = tailletextepardefaut;
+        texte.style.fontSize = tailletextepardefaut + 'px';
+        inputTaille.value = tailleActuelle.textContent;
+    }
+});
+
+inputTaille.addEventListener('input', (e) => {
+    let tailledutexte = e.target.value;
+
+    if (tailledutexte >= 8 && tailledutexte <= 48) {
+        tailleActuelle.textContent = tailledutexte;
         texte.style.fontSize = tailledutexte + 'px';
     }
     else {
-        document.querySelector('#px').textContent = tailletextepardefaut;
+        inputTaille.value = tailletextepardefaut;
+        tailleActuelle.textContent = tailletextepardefaut;
         texte.style.fontSize = tailletextepardefaut + 'px';
     }
 });
