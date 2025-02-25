c0154f6e85e991af2b89c6ec943ade327adf05e4
llissandreCRM <118750600+llissandre@users.noreply.github.com>
2025-02-25 12:55:56 +0100

    exercice formulaire en cours

--- 02 - webfront/JS-Intro/Exercices/Compteur de clicks/assets/index.js
+++ 02 - webfront/JS-Intro/Exercices/Compteur de clicks/assets/index.js
@@ -1,9 +1,11 @@
 const btnajout = document.querySelector('#btnajout');
 const btnreinitialiser = document.querySelector('#btnreinitialiser');
-const span = document.getElementsByTagName('span');
+const span = document.querySelector('span');
+
 
 btnajout.addEventListener('click', function () {
     let nbclick = Number(document.querySelector('span').textContent);
+    span.textContent = nbclick + 1;
     document.querySelector('span').textContent = nbclick + 1;
 });
 
---
+++ 02 - webfront/JS-Intro/Exercices/Formulaires/assets/js/index.js
@@ -0,0 +1,53 @@
+const form = document.querySelector('#form');
+const prenom = document.querySelector('#prenom');
+const age = document.querySelector('#age');
+const vider = document.querySelector('#reset');
+const sentences = document.querySelector('#sentence');
+
+form.addEventListener('submit', (e) => {
+    if (prenom.value != '' && age.value > 0) {
+        e.preventDefault();
+        
+        sentences.innerHTML = '<li>Bonjour ' + prenom.value + ', votre âge est : ' + age.value + ' ans.</li>';
+
+        if (age.value >= 18) {
+            sentences.innerHTML += '<li>Vous êtes majeur.</li>';
+        }
+        else {
+            sentences.innerHTML += '<li>Vous êtes mineur.</li>';
+        }
+
+        if (age.value < 64) {
+            let nbAnnees = 64 - age.value;
+            sentences.innerHTML += '<li>Il vous reste ' + nbAnnees + ' année(s) avant la retraite.</li>';
+        }
+        else if (age.value > 64) {
+            let nbAnnees = age.value - 64;
+            sentences.innerHTML += '<li>Vous êtes à la retraite depuis ' + nbAnnees + ' année(s).</li>';
+        }
+        else if (age.value = 64) {
+            sentences.innerHTML += '<li>Vous prenez votre retraite cette année !</li>';
+        }
+    }
+    else {
+        e.preventDefault();
+        sentences.innerHTML = '<li>Compléter/corriger le formulaire</li>';
+    }
+});
+
+reset.addEventListener('click', () => {
+    sentences.remove();
+})
+
+
+// window.addEventListener('load', getSentences());
+
+// function storeSentence() {
+//     window.localStorage.sentences = sentences.textContent;
+// }
+
+// function getSentences() {
+//     if (window.localStorage.sentences) {
+//         sentences.textContent = window.localStorage.sentences;
+//     }
+// }
