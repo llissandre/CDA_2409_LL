7e052fbb22f6e43617da938e23c8ee3eac2d6369
llissandre <118750600+llissandre@users.noreply.github.com>
2025-02-25 17:35:02 +0100

    Exercice "Introduction aux dates" en cours

---
+++ 02 - webfront/JS-Intro/Exercices/Introduction aux dates/assets/js/index.js
@@ -0,0 +1,6 @@
+const form = document.querySelector('form');
+const dateNaissance = document.querySelector('#dateNaissance');
+const calculer = document.querySelector('#calculer');
+const sentences = document.querySelector('#sentence');
+
+sentences.innerHTML = '<li>Sélectionner un date dans le passé.</li>';
