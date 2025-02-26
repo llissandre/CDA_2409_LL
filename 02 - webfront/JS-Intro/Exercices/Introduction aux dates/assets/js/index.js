0fd5c2f45e254b7d629aa4691eec7b52f8b27985
llissandre <118750600+llissandre@users.noreply.github.com>
2025-02-26 13:41:03 +0100

    Fin de l'exercice introduction aux dates

--- 02 - webfront/JS-Intro/Exercices/Introduction aux dates/assets/js/index.js
+++ 02 - webfront/JS-Intro/Exercices/Introduction aux dates/assets/js/index.js
@@ -1,47 +1,71 @@
-const form = document.querySelector('form');
 const dateN = document.querySelector('#dateN');
 const calculer = document.querySelector('#calculer');
 const sentences = document.querySelector('#sentence');
-const dateT = new Date().toLocaleString();
 const dateToday = new Date().toISOString().split('T')[0];
 const anneeToday = new Date().toISOString().split('T')[0].split('-')[0];
-const hourToday = new Date().toISOString().split('T')[1].split('.')[0];
 
 sentences.innerHTML = '<li>Sélectionner un date dans le passé.</li>';
 
 calculer.addEventListener('click', () => {
     const dateDeN = dateN.value;
-    // const dateDeND = dateDeN.split('T')[0];
-    // const dateDeNH = dateDeN.split('T')[1];
-    // const dateDeNDAnnee = dateDeND.split('-')[0];
-    // const dateDeNDMois = dateDeND.split('-')[1];
-    // const dateDeNDJour = dateDeND.split('-')[2];
     const dateDeNA = new Date(dateDeN);
-    const dateDeNT = new Date(dateDeN).toLocaleString();
     const dateND = new Date(dateDeN).toLocaleDateString();
     const dateNT = new Date(dateDeN).toLocaleTimeString();
 
     const anneeNaissance = dateDeNA.getFullYear();
     const moisNaissance = dateDeNA.getMonth() + 1;
     const jourNaissance = dateDeNA.getDate();
-    
+
     let nbAnnees = anneeToday - anneeNaissance;
-    let signeAtro = '';
-    
+    let signeAstro = '';
+
     if (dateDeN > dateToday || dateDeN == '') {
         sentences.innerHTML = '<li>Vous devez sélectionner une date valide, ou une date dans le passé!</li>';
     }
     else {
         sentences.innerHTML = '<li>Vous êtes né le <span class="colorBlue">' + dateND + '</span> à <span class="colorBlue">' + dateNT + '</span>.</li>';
         sentences.innerHTML += '<li>Il s\'est écoulé ' + nbAnnees + ' année(s) depuis votre naissance.</li>';
-        
+
         sentences.innerHTML += '<hr>';
-        
+
         if ((jourNaissance >= 20 && moisNaissance == 1) || (jourNaissance <= 18 && moisNaissance == 2)) {
-            signeAstro = "Verseau";
+            signeAstro = "verseau";
+        }
+        else if ((jourNaissance >= 19 && moisNaissance == 2) || (jourNaissance <= 20 && moisNaissance == 3)) {
+            signeAstro = "poisson";
+        }
+        else if ((jourNaissance >= 21 && moisNaissance == 3) || (jourNaissance <= 19 && moisNaissance == 4)) {
+            signeAstro = "bélier";
+        }
+        else if ((jourNaissance >= 20 && moisNaissance == 4) || (jourNaissance <= 20 && moisNaissance == 5)) {
+            signeAstro = "taureau";
+        }
+        else if ((jourNaissance >= 21 && moisNaissance == 5) || (jourNaissance <= 20 && moisNaissance == 6)) {
+            signeAstro = "gémeau";
+        }
+        else if ((jourNaissance >= 21 && moisNaissance == 6) || (jourNaissance <= 22 && moisNaissance == 7)) {
+            signeAstro = "cancer";
+        }
+        else if ((jourNaissance >= 23 && moisNaissance == 7) || (jourNaissance <= 22 && moisNaissance == 8)) {
+            signeAstro = "lion";
+        }
+        else if ((jourNaissance >= 23 && moisNaissance == 8) || (jourNaissance <= 22 && moisNaissance == 9)) {
+            signeAstro = "vierge";
+        }
+        else if ((jourNaissance >= 23 && moisNaissance == 9) || (jourNaissance <= 22 && moisNaissance == 10)) {
+            signeAstro = "balance";
+        }
+        else if ((jourNaissance >= 23 && moisNaissance == 10) || (jourNaissance <= 21 && moisNaissance == 11)) {
+            signeAstro = "scorpion";
+        }
+        else if ((jourNaissance >= 22 && moisNaissance == 11) || (jourNaissance <= 21 && moisNaissance == 12)) {
+            signeAstro = "sagittaire";
+        }
+        else if ((jourNaissance >= 22 && moisNaissance == 12) || (jourNaissance <= 19 && moisNaissance == 1)) {
+            signeAstro = "capricorne";
         }
 
-        sentences.innerHTML += '<li>Votre signe astrologique : <span class:"colorBlue">' + signeAtro + '</span>.</li>';
+        sentences.innerHTML += '<li>Votre signe astrologique : <span class="colorBlue">' + signeAstro + '</span>.</li>';
 
     }
 });
