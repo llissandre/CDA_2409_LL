7ac49c6e26d57bce27e3eae08310e3e8862895f5
llissandre <118750600+llissandre@users.noreply.github.com>
2025-02-26 21:19:11 +0100

    Début de la partie affiner du l'exercice intervalles temporels

--- 02 - webfront/JS-Intro/Exercices/Intervalles temporels/assets/js/index.js
+++ 02 - webfront/JS-Intro/Exercices/Intervalles temporels/assets/js/index.js
@@ -1,16 +1,40 @@
-const dateInput = document.querySelector('#date');
-const timeInput = document.querySelector('#time');
+const currentdateInput = document.querySelector('#currentdate');
+const intervalledate = document.querySelector('#intervalledate');
+const currenttimeInput = document.querySelector('#currenttime');
 const afficher = document.querySelector('#afficher');
+const calculerIntervalle = document.querySelector('#calculerIntervalle');
+const dateTodayLongue = new Date();
 const dateToday = new Date().toISOString().split('T')[0];
 const timeToday = new Date().toISOString().split('T')[1].split('.')[0];
-const sentences = document.querySelector('#sentence');
+const sentencesCurrentdate = document.querySelector('#sentenceCurrentdate');
+const sentencesIntervalledate = document.querySelector('#sentenceIntervalledate');
 
-afficher.addEventListener('click', () => {
-    dateInput.value = dateToday;
-    let dateAujourdhui = new Date(dateToday).toLocaleDateString();
-    timeInput.value = timeToday;
-    console.log(dateInput.value);
-    sentences.innerHTML += '<hr>';
-    sentences.innerHTML += '<li>Aujourd\'hui, nous sommes le <span class="colorBlue">' + dateAujourdhui + '</span>, l\'heure courante est : <span class="colorBlue">' + timeToday + '</span> .</li>';
-    sentences.innerHTML += '<hr>';
+const dateDuJour = () => {
+    currentdateInput.value = dateToday;
+    let currentdate = new Date(dateToday).toLocaleDateString();
+    // console.log(currentdateInput.value);
+    // console.log(dateToday);
+    currenttimeInput.value = timeToday;
+    sentencesCurrentdate.innerHTML += '<hr>';
+    sentencesCurrentdate.innerHTML += '<li>Aujourd\'hui, nous sommes le <span class="colorBlue">' + currentdate + '</span>, l\'heure courante est : <span class="colorBlue">' + timeToday + '</span> .</li>';
+};
+
+
+calculerIntervalle.addEventListener('click', () => {
+    const intervalledateValue = intervalledate.value;
+    const intervalledateLongue = new Date(intervalledateValue);
+    const intervalledateD = new Date(intervalledateValue).toLocaleDateString();
+    const intervalledateT = new Date(intervalledateValue).toLocaleTimeString();
+    const annee = intervalledateLongue.getFullYear();
+    const mois = intervalledateLongue.getMonth() + 1;
+    const jour = intervalledateLongue.getDate();
+    const heure = intervalledateLongue.getHours();
+    const minute = intervalledateLongue.getMinutes();
+
+    let difference = Math.abs(intervalledateLongue - dateTodayLongue);
+    let intervalle = Math.round(difference / (1000 * 60 * 60 * 24));
+
+    sentencesIntervalledate.innerHTML = '<li>Il y a ' + intervalle + ' jour(s) entre aujourd\'hui et le <span class="colorBlue">' + intervalledateD + ' à ' + intervalledateT + '</span> .</li>';
 });
+
+afficher.addEventListener('click', dateDuJour);
--- 02 - webfront/JS-Intro/Exercices/Introduction aux dates/assets/js/index.js
+++ 02 - webfront/JS-Intro/Exercices/Introduction aux dates/assets/js/index.js
@@ -1,14 +1,15 @@
 const dateN = document.querySelector('#dateN');
 const calculer = document.querySelector('#calculer');
-const sentences = document.querySelector('#sentence');
+const sentencesDateDuJour = document.querySelector('#sentence');
 const dateToday = new Date().toISOString().split('T')[0];
 const anneeToday = new Date().toISOString().split('T')[0].split('-')[0];
 
-sentences.innerHTML = '<li>Sélectionner un date dans le passé.</li>';
+sentencesDateDuJour.innerHTML = '<li>Sélectionner un date dans le passé.</li>';
 
 const astro = () => {
     const dateDeN = dateN.value;
     const dateDeNA = new Date(dateDeN);
+
     const dateND = new Date(dateDeN).toLocaleDateString();
     const dateNT = new Date(dateDeN).toLocaleTimeString();
 
@@ -20,13 +21,13 @@ const astro = () => {
     let signeAstro = '';
 
     if (dateDeN > dateToday || dateDeN == '') {
-        sentences.innerHTML = '<li>Vous devez sélectionner une date valide, ou une date dans le passé!</li>';
+        sentencesDateDuJour.innerHTML = '<li>Vous devez sélectionner une date valide, ou une date dans le passé!</li>';
     }
     else {
-        sentences.innerHTML = '<li>Vous êtes né le <span class="colorBlue">' + dateND + '</span> à <span class="colorBlue">' + dateNT + '</span>.</li>';
-        sentences.innerHTML += '<li>Il s\'est écoulé ' + nbAnnees + ' année(s) depuis votre naissance.</li>';
+        sentencesDateDuJour.innerHTML = '<li>Vous êtes né le <span class="colorBlue">' + dateND + '</span> à <span class="colorBlue">' + dateNT + '</span>.</li>';
+        sentencesDateDuJour.innerHTML += '<li>Il s\'est écoulé ' + nbAnnees + ' année(s) depuis votre naissance.</li>';
 
-        sentences.innerHTML += '<hr>';
+        sentencesDateDuJour.innerHTML += '<hr>';
 
         if ((jourNaissance >= 20 && moisNaissance == 1) || (jourNaissance <= 18 && moisNaissance == 2)) {
             signeAstro = "verseau";
@@ -65,7 +66,7 @@ const astro = () => {
             signeAstro = "capricorne";
         }
 
-        sentences.innerHTML += '<li>Votre signe astrologique : <span class="colorBlue">' + signeAstro + '</span>.</li>';
+        sentencesDateDuJour.innerHTML += '<li>Votre signe astrologique : <span class="colorBlue">' + signeAstro + '</span>.</li>';
 
     }
 };
