9ebf28325d262aaaf3496038eb567514f3e3989b
llissandre <118750600+llissandre@users.noreply.github.com>
2025-03-03 01:41:46 +0100

    mt en cours

--- 02 - webfront/JS-Intro/Exercices/Manipuler un tableau/assets/js/index.js
+++ 02 - webfront/JS-Intro/Exercices/Manipuler un tableau/assets/js/index.js
@@ -45,20 +45,44 @@ tr.append(thSupprimer);
 table.append(tbody);
 
 for (let i = 0; i < people.length; i++) {
-    let tr = document.createElement('tr');
-    tbody.appendChild(tr);
-    
-    for (let j = 0; j < 2; j++) {
-        let td = document.createElement('td');
-        // let firstname = people[i].split(' ')[j];
-        // let lastname = people[i].split(' ')[j];
-        td.textContent = people[i].split(' ')[j];
-        tr.appendChild(td);
-    }
+    const trpeople = document.createElement('tr');
+    const tdlastname = document.createElement('td');
+    const tdfirstname = document.createElement('td');
+    const tdemail = document.createElement('td');
+    const tddelete = document.createElement('td');
+
+    tdlastname.textContent = people[i].split(' ')[1];
+    tdfirstname.textContent = people[i].split(' ')[0];
+    tdemail.textContent = people[i].split(' ')[0].toLowerCase() + '.' + people[i].split(' ')[1].toLowerCase() + '@example.com';
+    tddelete.innerHTML = '<p id="delete">X</p>';
 
 
+    tbody.append(trpeople);
+    trpeople.append(tdlastname);
+    trpeople.append(tdfirstname);
+    trpeople.append(tdemail);
+    trpeople.append(tddelete);
 }
 
+const deleteid = document.querySelector('#delete');
+
+deleteid.addEventListener('click', (e) => {
+    if (e.target.classList.contains('checked')) {
+        e.target.remove();
+    }
+    else {
+        e.target.classList.add('checked');
+    }
+
+});
+
+// for (let j = 0; j < 2; j++) {
+//     let td = document.createElement('td');
+//     // let firstname = people[i].split(' ')[j];
+//     // let lastname = people[i].split(' ')[j];
+//     td.textContent = people[i].split(' ')[j];
+//     tr.appendChild(td);
+// }
 
 
 
