02121a49d3263561517ed888e5a604523722db3b
llissandreCRM <118750600+llissandre@users.noreply.github.com>
2025-03-03 16:09:39 +0100

    Manipuler la collection en cours

--- 02 - webfront/JS-Intro/Exercices/Manipuler un tableau/assets/js/index.js
+++ 02 - webfront/JS-Intro/Exercices/Manipuler un tableau/assets/js/index.js
@@ -14,22 +14,26 @@ const labelPrenomAdd = document.createElement('label');
 const labelNomAdd = document.createElement('label');
 const inputPrenomAdd = document.createElement('input');
 const inputNomAdd = document.createElement('input');
-const inputAjouterAdd = document.createElement('input');
+const btnAjouterAdd = document.createElement('input');
 
 divAdd.setAttribute('id', 'divAddId');
 labelPrenomAdd.setAttribute('type', 'text');
+labelPrenomAdd.setAttribute('for', 'PrenomAdd');
 labelNomAdd.setAttribute('type', 'text');
+labelNomAdd.setAttribute('for', 'NomAdd');
 inputPrenomAdd.setAttribute('type', 'text');
+inputPrenomAdd.setAttribute('id', 'PrenomAdd');
 inputNomAdd.setAttribute('type', 'text');
-inputAjouterAdd.setAttribute('type', 'submit');
+inputNomAdd.setAttribute('id', 'NomAdd');
+btnAjouterAdd.setAttribute('type', 'submit');
 
 inscrit.before(divAdd);
 
 pAddEntete.textContent = 'Ajouter une personne';
 labelPrenomAdd.textContent = 'Prénom: ';
 labelNomAdd.textContent = 'Nom: ';
-inputAjouterAdd.value = 'Ajouter';
-pAddEntete.textContent += 'ajouté !';
+btnAjouterAdd.value = 'Ajouter';
+pAddResultat.textContent = '';
 
 divAdd.append(pAddEntete);
 divAdd.append(form);
@@ -37,83 +41,95 @@ form.append(labelPrenomAdd);
 form.append(inputPrenomAdd);
 form.append(labelNomAdd);
 form.append(inputNomAdd);
-form.append(inputAjouterAdd);
+form.append(btnAjouterAdd);
 divAdd.append(pAddResultat);
 
-// const personnes = () => {
-//     table.innerText = '';
-//     inscrit.innerText = '';
+btnAjouterAdd.addEventListener('click', (e) => {
+    console.log(e);
+    e.preventDefault();
+    const prenom = `${String(inputPrenomAdd.value).charAt(0).toUpperCase()}${String(inputPrenomAdd.value).slice(1).toLowerCase()}`;
+    const nom = `${String(inputNomAdd.value).charAt(0).toUpperCase()}${String(inputNomAdd.value).slice(1).toLowerCase()}`;
 
-//     if (people.length > 0) {
-//         const divTab = document.createElement('div');
-//         const divTabId = document.querySelector('#divTab');
-//         const tableId = document.querySelector('#table');
-//         const thead = document.createElement('thead');
-//         const tr = document.createElement('tr');
-//         const thNom = document.createElement('th');
-//         const thPrenom = document.createElement('th');
-//         const thEmail = document.createElement('th');
-//         const thSupprimer = document.createElement('th');
-//         const tbody = document.createElement('tbody');
+    people.push(`${prenom} ${nom}`);
+
+    pAddResultat.textContent = `${prenom} ${nom} ajouté !`;
+
+    personnes();
+});
+
+const personnes = () => {
+    table.innerText = '';
+    inscrit.innerText = '';
+
+    if (people.length > 0) {
+        const divTab = document.createElement('div');
+        const divTabId = document.querySelector('#divTab');
+        const tableId = document.querySelector('#table');
+        const thead = document.createElement('thead');
+        const tr = document.createElement('tr');
+        const thNom = document.createElement('th');
+        const thPrenom = document.createElement('th');
+        const thEmail = document.createElement('th');
+        const thSupprimer = document.createElement('th');
+        const tbody = document.createElement('tbody');
 
 
-//         thNom.textContent = 'Nom';
-//         thPrenom.textContent = 'Prénom';
-//         thEmail.textContent = 'Email';
-//         thSupprimer.textContent = 'Supprimer';
+        thNom.textContent = 'Nom';
+        thPrenom.textContent = 'Prénom';
+        thEmail.textContent = 'Email';
+        thSupprimer.textContent = 'Supprimer';
 
-//         divTab.setAttribute('id', 'divTabId');
-//         table.setAttribute('id', 'tableId');
+        divTab.setAttribute('id', 'divTabId');
+        table.setAttribute('id', 'tableId');
 
-//         filet.append(divTab);
-//         divTab.append(table);
-//         table.append(thead);
-//         thead.append(tr);
-//         tr.append(thNom);
-//         tr.append(thPrenom);
-//         tr.append(thEmail);
-//         tr.append(thSupprimer);
-//         table.append(tbody);
+        filet.append(divTab);
+        divTab.append(table);
+        table.append(thead);
+        thead.append(tr);
+        tr.append(thNom);
+        tr.append(thPrenom);
+        tr.append(thEmail);
+        tr.append(thSupprimer);
+        table.append(tbody);
 
-//         people.forEach((element, i) => {
-//             // for (let i = 0; i < people.length; i++) {
-//             const peoplelist = document.createElement('li');
-//             const trpeople = document.createElement('tr');
-//             const tdlastname = document.createElement('td');
-//             const tdfirstname = document.createElement('td');
-//             const tdemail = document.createElement('td');
-//             const tddelete = document.createElement('td');
+        people.forEach((element, i) => {
+            // for (let i = 0; i < people.length; i++) {
+            const peoplelist = document.createElement('li');
+            const trpeople = document.createElement('tr');
+            const tdlastname = document.createElement('td');
+            const tdfirstname = document.createElement('td');
+            const tdemail = document.createElement('td');
+            const tddelete = document.createElement('td');
 
-//             peoplelist.textContent = element;
+            peoplelist.textContent = element;
+            tdlastname.textContent = element.split(' ')[1];
+            tdfirstname.textContent = element.split(' ')[0];
+            tdemail.textContent = element.split(' ')[0].toLowerCase() + '.' + element.split(' ')[1].toLowerCase() + '@example.com';
+            tddelete.innerHTML = `<p id="${i}" class="delete">X</p>`;
 
-//             tdlastname.textContent = element.split(' ')[1];
-//             tdfirstname.textContent = element.split(' ')[0];
-//             tdemail.textContent = element.split(' ')[0].toLowerCase() + '.' + element.split(' ')[1].toLowerCase() + '@example.com';
-//             tddelete.innerHTML = `<p id="${i}" class="delete">X</p>`;
+            inscrit.append(peoplelist);
+            tbody.append(trpeople);
+            trpeople.append(tdlastname);
+            trpeople.append(tdfirstname);
+            trpeople.append(tdemail);
+            trpeople.append(tddelete);
+        });
 
-//             inscrit.append(peoplelist);
-//             tbody.append(trpeople);
-//             trpeople.append(tdlastname);
-//             trpeople.append(tdfirstname);
-//             trpeople.append(tdemail);
-//             trpeople.append(tddelete);
-//         });
+        const deletecl = document.querySelectorAll('.delete');
 
-//         const deletecl = document.querySelectorAll('.delete');
+        deletecl.forEach((element, i) => {
+            element.addEventListener('click', (e) => {
+                if (e.target.classList.contains('checked')) {
+                    people.splice(i, 1)
+                    personnes();
+                }
+                else {
+                    e.target.classList.add('checked');
+                    e.target.innerHTML = ' ';
+                }
+            });
+        });
+    }
+}
 
-//         deletecl.forEach((element, i) => {
-//             element.addEventListener('click', (e) => {
-//                 if (e.target.classList.contains('checked')) {
-//                     people.splice(i, 1)
-//                     personnes();
-//                 }
-//                 else {
-//                     e.target.classList.add('checked');
-//                     e.target.innerHTML = ' ';
-//                 }
-//             });
-//         });
-//     }
-// }
-
-// personnes();
+personnes();
