a97e4ec12e6e8741252667f4093dd48dda260048
llissandreCRM <118750600+llissandre@users.noreply.github.com>
2025-03-21 11:55:42 +0100

    Exercice cereals en cours

---
+++ 02 - webfront/JS-Intro/Exercices/Céréales/assets/js/index.js
@@ -0,0 +1,58 @@
+const searchCereal = document.getElementById('searchCereal');
+const myTable = document.getElementById('myTable');
+const tbody = document.createTBody();
+const tfoot = document.createTFoot();
+
+async function fetchCereals() {
+    try {
+        const reponse = await fetch('./assets/json/cereals.json');
+        if (!reponse.ok) {
+            throw new Error('La réponse n\'est pas ok')
+        }
+        data = await reponse.json();
+        display();
+    } catch (error) {
+        console.error('Un problème est survenu lors de la récupération : ', erreur);
+        data = [];
+    }
+}
+
+const display = () => {
+    tbody.textContent = '';
+    tfoot.textContent = '';
+
+    if (data.length > 0) {
+        myTable.append(tfoot);
+
+        data.forEach((cereal) => {
+            const trTBody = tbody.insertRow();
+            let cellId = trTBody.insertCell();
+            cellId.textContent = cereal.id;
+            let cellNom = trTBody.insert();
+            cellNom.textContent = cereal.name;
+            let cellCalories = trTBody.insertCell();
+            cellCalories.textContent = cereal.calories;
+            let cellProteine = trTBody.insertCell();
+            cellProteine.textContent = cereal.protein;
+            let cellSel = trTBody.insertCell();
+            cellSel.textContent = cereal.sodium;
+            let cellFibres = trTBody.insertCell();
+            cellFibres.textContent = cereal.fiber;
+            let cellGlucides = trTBody.insertCell();
+            cellGlucides.textContent = cereal.carbo;
+            let cellSucre = trTBody.insertCell();
+            cellSucre.textContent = cereal.sugars;
+            let cellPotassium = trTBody.insertCell();
+            cellPotassium.textContent = cereal.potass;
+            let cellVitamines = trTBody.insertCell();
+            cellVitamines.textContent = cereal.vitamins;
+            let cellEvaluation = trTBody.insertCell();
+            cellEvaluation.textContent = cereal.rating;
+
+        });
+    }
+    else { }
+}
+
+
+fetchCereals();
