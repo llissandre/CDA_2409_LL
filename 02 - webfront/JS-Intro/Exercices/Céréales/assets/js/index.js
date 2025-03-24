95685d6110336271caffd0b050541b2ca9f86165
llissandre <118750600+llissandre@users.noreply.github.com>
2025-03-24 23:33:12 +0100

    Exercice en cours

--- 02 - webfront/JS-Intro/Exercices/Céréales/assets/js/index.js
+++ 02 - webfront/JS-Intro/Exercices/Céréales/assets/js/index.js
@@ -3,12 +3,14 @@ let nutriscoreChecked = [];
 
 const inputSearchCereal = document.getElementById('inputSearchCereal');
 const myTable = document.getElementById('myTable');
-const inputCheckboxes = document.querySelector('input[type="checkbox"]');
-const a = document.getElementById('a');
-const b = document.getElementById('b');
-const c = document.getElementById('c');
-const d = document.getElementById('d');
-const e = document.getElementById('e');
+const inputCheckboxes = document.querySelectorAll('.ns input')
+const filters = document.querySelectorAll('.fieldsetF');
+const all = document.getElementById('all').selected;
+const withoutSugar = document.getElementById('withoutSugar').selected;
+const lowSalt = document.getElementById('lowSalt').selected;
+const boost = document.getElementById('boost').selected;
+
+const removed = new Set();
 
 const tbody = myTable.createTBody();
 const tfoot = myTable.createTFoot();
@@ -30,88 +32,90 @@ async function fetchCereals() {
     }
 }
 
-const display = (result) => {
+const display = (collectionCereals) => {
     tbody.textContent = '';
     tfoot.textContent = '';
 
-    if (result.length > 0) {
+    if (collectionCereals.length > 0) {
         myTable.append(tfoot);
 
-        result.forEach((cereal) => {
-            const trTBody = tbody.insertRow();
-            let cellId = trTBody.insertCell();
-            cellId.textContent = cereal.id;
-            let cellNom = trTBody.insertCell();
-            cellNom.textContent = cereal.name;
-            let cellCalories = trTBody.insertCell();
-            cellCalories.textContent = cereal.calories;
-            let cellProteine = trTBody.insertCell();
-            cellProteine.textContent = cereal.protein;
-            let cellSel = trTBody.insertCell();
-            cellSel.textContent = cereal.sodium;
-            let cellFibres = trTBody.insertCell();
-            cellFibres.textContent = cereal.fiber;
-            let cellGlucides = trTBody.insertCell();
-            cellGlucides.textContent = cereal.carbo;
-            let cellSucre = trTBody.insertCell();
-            cellSucre.textContent = cereal.sugars;
-            let cellPotassium = trTBody.insertCell();
-            cellPotassium.textContent = cereal.potass;
-            let cellVitamines = trTBody.insertCell();
-            cellVitamines.textContent = cereal.vitamins;
-            let cellEvaluation = trTBody.insertCell();
-            cellEvaluation.textContent = cereal.rating;
+        collectionCereals.forEach(cereal => {
+            if (!removed.has(cereal.id)) {
+                const trTBody = tbody.insertRow();
+                let cellId = trTBody.insertCell();
+                cellId.textContent = cereal.id;
+                let cellNom = trTBody.insertCell();
+                cellNom.textContent = cereal.name;
+                let cellCalories = trTBody.insertCell();
+                cellCalories.textContent = cereal.calories;
+                let cellProteine = trTBody.insertCell();
+                cellProteine.textContent = cereal.protein;
+                let cellSel = trTBody.insertCell();
+                cellSel.textContent = cereal.sodium;
+                let cellFibres = trTBody.insertCell();
+                cellFibres.textContent = cereal.fiber;
+                let cellGlucides = trTBody.insertCell();
+                cellGlucides.textContent = cereal.carbo;
+                let cellSucre = trTBody.insertCell();
+                cellSucre.textContent = cereal.sugars;
+                let cellPotassium = trTBody.insertCell();
+                cellPotassium.textContent = cereal.potass;
+                let cellVitamines = trTBody.insertCell();
+                cellVitamines.textContent = cereal.vitamins;
+                let cellEvaluation = trTBody.insertCell();
+                cellEvaluation.textContent = cereal.rating;
 
-            calculateNutriscore(cereal.rating);
-            let cellNS = trTBody.insertCell();
-            cellNS.textContent = nutriscore;
-            cellNS.style.backgroundColor = color;
+                getNS(cereal.rating);
+                let cellNS = trTBody.insertCell();
+                cellNS.textContent = nutriscore;
+                cellNS.style.backgroundColor = color;
 
-            let cellDEL = trTBody.insertCell();
-            cellDEL.innerHTML = '&#128938;';
-            cellDEL.classList.add('pointer');
-            cellDEL.style.color = 'red';
+                let cellDEL = trTBody.insertCell();
+                cellDEL.innerHTML = '&#128938;';
+                cellDEL.classList.add('pointer');
+                cellDEL.style.color = 'red';
 
-            cellDEL.addEventListener('click', () => {
-                const indexOfCereal = result.indexOf(cereal);
-                result.splice(indexOfCereal, 1)
-                display(result);
-            })
+                cellDEL.addEventListener('click', () => {
+                    // const indexOfCereal = collectionCereals.indexOf(cereal);
+                    // collectionCereals.splice(indexOfCereal, 1)
+                    removed.add(cereal.id);
+                    display(collectionCereals);
+                })
+            }
         });
     }
 
     const trTFoot = tfoot.insertRow();
-    const sum = result.reduce((a, b) => a + b.calories, 0);
-    let avgCalories = Math.floor(sum / result.length);
+    const sum = collectionCereals.reduce((a, b) => a + b.calories, 0);
+    let avgCalories = Math.floor(sum / collectionCereals.length);
 
-    if (result.length === 0) {
+    if (collectionCereals.length === 0) {
         avgCalories = 0;
     }
 
     let cellIdVide = trTFoot.insertCell();
     cellIdVide.textContent = '';
     let cellNbElements = trTFoot.insertCell();
-    cellNbElements.textContent = result.length + ' éléments';
+    cellNbElements.textContent = collectionCereals.length + ' éléments';
     let cellAvgCalories = trTFoot.insertCell();
-    cellAvgCalories.innerHTML = 'Moyenne calories : ' + avgCalories;
-
+    cellAvgCalories.textContent = 'Moyenne calories : ' + avgCalories;
 }
 
 const searchCereals = (e) => {
     e.preventDefault();
 
-    let resultCereals = collectionCereals.filter((cereal) =>
+    let result = collectionCereals.filter(cereal =>
         cereal.name.trim().toLowerCase().includes(inputSearchCereal.value.trim().toLowerCase()));
 
-    if (resultCereals.length === 0) {
+    if (result.length === 0) {
         alert('Aucune correspondance n\'a été trouvée');
     }
     else {
-        display(resultCereals)
+        display(result)
     };
 };
 
-const calculateNutriscore = (rating) => {
+const getNS = (rating) => {
     if (rating < 35) {
         nutriscore = 'E';
         color = 'var(--E)'
@@ -136,14 +140,18 @@ const calculateNutriscore = (rating) => {
 
 inputSearchCereal.addEventListener('search', searchCereals);
 
-
-
-d.addEventListener(('change'), (e) => {
-
+inputCheckboxes.forEach(ns => ns.addEventListener('change', (e) => {
     if (e.target.checked) {
-        console.log('test');
+        nutriscoreChecked.push(e.target.id);
+    }
+    else {
+        let myIndex = nutriscoreChecked.indexOf(e.target.id);
+        nutriscoreChecked.splice(myIndex, 1);
     }
 
-});
+    console.log(nutriscoreChecked);
+}));
+
+// filters.forEach(ns => ns.addEventListener('change', display));
 
 fetchCereals();
