ddb828d384bf39969b603fcfc8629d59bf93404b
llissandre <118750600+llissandre@users.noreply.github.com>
2025-03-27 21:40:02 +0100

    exercice évaluation avec VueJS en cours

--- 02 - webfront/JS-Intro/Exercices/Évaluation - VueJS/assets/js/index.js
+++ 02 - webfront/JS-Intro/Exercices/Évaluation - VueJS/assets/js/index.js
@@ -1,74 +1,37 @@
-
-
-
-const filet = document.querySelector('#filet');
-const lastnameFirstnameInput = document.querySelector('#lastnameFirstnameInput');
-const gradeInput = document.querySelector('#gradeInput');
-const btnValidate = document.querySelector('#btnValider');
-const classMyTable = document.querySelector('.myTable');
-const myTable = document.querySelector('#myTable');
-const regexUsername = /^[a-zA-Z-]{2,}$/;
-
-let data = [];
-let failingGrade = 12;
-
-async function fetchEvaluation() {
-    try {
-        const response = await fetch('./assets/json/eval.json');
-        if (!response.ok) {
-            throw new Error('La réponse n\'est pas OK');
+const appEvaluation = {
+    data() {
+        return {
+            listEvaluations: [],
+            failingGrade: 12,
+            nbEtudiants: 0,
+            avgClasse: 0,
+            overAvg: 0,
+            overAvgGrades: 0,
+            usernameInput: '',
+            grade: 0,
+            fullname: '',
+            regexUsername: /^[a-zA-Z-]{2,}$/
         }
-        data = await response.json();
-        display();
-    }
-    catch (error) {
-        console.error('Un problème est survenu lors de la récupération :', error);
-    }
-}
-
-let nbEtudiants;
-
-const display = () => {
-
-    myDivul.setAttribute('class', 'myDivulClass');
-
-    data.sort((a, b) => b.grade - a.grade);
-    nbEtudiants = data.length;
-    let sum = data.reduce((sum, a) => sum + a.grade, 0);
-    let avgClasse = sum / nbEtudiants;
-    let overAvg = data.filter(a => a.grade >= 10);
-    let overAvgGrades = data.filter(a => a.grade >= avgClasse);
-
-    data.forEach((element) => {
-        const trTBody = tbody.insertRow();
-        let cellLastname = trTBody.insertCell();
-        cellLastname.innerText = String(element.fullname).split(' ')[0].charAt(0).toUpperCase() + String(element.fullname).split(' ')[0].slice(1).toLowerCase();
-        let cellFirstname = trTBody.insertCell();
-        cellFirstname.innerText = String(element.fullname).split(' ')[1].charAt(0).toUpperCase() + String(element.fullname).split(' ')[1].slice(1).toLowerCase();
-        let cellGrade = trTBody.insertCell();
-        cellGrade.innerText = element.grade;
-        let cellObtenu = trTBody.insertCell();
-        element.grade >= failingGrade ? cellObtenu.innerText = 'Oui' : cellObtenu.innerText = 'non';
-    });
-}
-
-const validate = () => {
-    e.preventDefault();
-
-    let usernameInput = lastnameFirstnameInput.value.trim();
-    let lastname = usernameInput.split(' ')[0];
-    let firstname = usernameInput.split(' ')[1];
-    grade = Number(gradeInput.value);
-    fullname = lastname + ' ' + firstname;
-
-    if (regexUsername.test(lastname) && lastname !== undefined && regexUsername.test(firstname) && firstname !== undefined && grade >= 0 && grade <= 20) {
-
-        data.push({ fullname, grade });
-        myTable.innerText = '';
-        
-        display();
+    },
+    async created() {
+        try {
+            let response = await fetch('./assets/json/eval.json');
+            let json = await response.json();
+            this.listEvaluations = json;
+        }
+        catch (error) {
+            console.error('Un problème est survenu lors de la récupération :', error);
+        }
+    },
+    computed: {
+    },
+    methods: {
+        getLastname(fullname) {
+            return fullname.split(' ')[0];
+        },
+        getFirstname(fullname) {
+            return fullname.split(' ')[1];
+        }
     }
 }
-
-fetchEvaluation();
-btnValidate.addEventListener('click', validate);
+Vue.createApp(appEvaluation).mount('#app');
