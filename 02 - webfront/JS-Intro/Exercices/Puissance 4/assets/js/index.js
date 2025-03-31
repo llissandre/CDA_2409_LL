e65288e7f2e8baf268295e2189412e9f3da1964b
llissandreCRM <118750600+llissandre@users.noreply.github.com>
2025-03-31 12:56:13 +0200

    L'exercice Puissance4 en VueJS

--- 02 - webfront/JS-Intro/Exercices/Puissance 4/assets/js/index.js
+++ 02 - webfront/JS-Intro/Exercices/Puissance 4/assets/js/index.js
@@ -1,1 +1,40 @@
-const 
+const appPuissanceQuatre = {
+    data() {
+        return {
+            player1: 'Rouge',
+            player2: 'Jaune',
+            currentPlayer: 'Rouge',
+            winner: null,
+            gameOver: false,
+        }
+    },
+    computed: {
+        getCurrentPlayer() {
+            return this.currentPlayer === 'Rouge' ? 'Rouge' : 'Jaune';
+        }
+    },
+    methods: {
+        startGame() {
+            this.currentPlayer = 'Rouge';
+            this.winner = null;
+            this.gameOver = false;
+        },
+        changePlayer() {
+            this.currentPlayer = this.currentPlayer === 'Rouge' ? 'Jaune' : 'Rouge';
+        },
+        checkWinner(player) {
+            if (this.$refs.grid.checkWin(player)) {
+                this.winner = player;
+                this.gameOver = true;
+            }
+        },
+        resetGame() {
+            this.startGame();
+        },
+        test(){
+            console.log(event);
+
+        }
+    }
+}
+Vue.createApp(appPuissanceQuatre).mount('#app');
