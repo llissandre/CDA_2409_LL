1c6b672d4c485e36043673aea141cb27f212dc81
llissandreCRM <118750600+llissandre@users.noreply.github.com>
2025-04-03 15:56:15 +0200

    Exercice P4 en cours

--- 02 - webfront/JS-Intro/Exercices/Puissance 4/assets/js/index.js
+++ 02 - webfront/JS-Intro/Exercices/Puissance 4/assets/js/index.js
@@ -55,7 +55,6 @@ const appPuissanceQuatre = {
 
             //             if (columnPlayer >= 0 && this.matrix[rowPlayer][columnPlayer] === this.getCurrentPlayer && counter < 4) {
             //                 counter++;
-
             //             }
             //             else {
             //                 j = 4;
@@ -72,57 +71,73 @@ const appPuissanceQuatre = {
             // rowPlayer = row;
             // columnPlayer = column;
 
-            // for (let i = 1; i < 4; i++) {
-            //     rowPlayer++;
-
-            //     if (rowPlayer <= this.nbRowOfP4 && this.matrix[rowPlayer][columnPlayer] === this.getCurrentPlayer && counter < 4) {
-            //         counter++;
-
-            //     }
-            //     else {
-            //         i = 4;
-            //         console.log(counter, i);
-            //     }
-            // }
-            
-            // if (counter === 4) {
-            //     alert('Victoire en vertical de ' + this.getCurrentPlayer + ', avec ' + counter + ' points.');
-            // }
-            
-            // counter = 1;
-            // rowPlayer = row;
-            // columnPlayer = column;
-            
             for (let i = 1; i < 4; i++) {
-                columnPlayer++;
                 rowPlayer++;
-                
-                if (columnPlayer <= this.nbColumnOfP4 && rowPlayer <= nbRowOfP4 && this.matrix[rowPlayer][columnPlayer] === this.getCurrentPlayer && counter < 4) {
+
+                if (rowPlayer <= this.nbRowOfP4 && this.matrix[rowPlayer][columnPlayer] === this.getCurrentPlayer && counter < 4) {
                     counter++;
+                    console.log(counter, i);
                 }
                 else {
                     i = 4;
                     console.log(counter, i);
 
-                    // let rowPlayer = row;
-                    // let columnPlayer = column;
+                    let rowPlayer = row;
+                    let columnPlayer = column;
 
-                    // for (let j = 0; j < 4; j++) {
-                    //     columnPlayer--;
+                    for (let j = 0; j < 4; j++) {
+                        rowPlayer--;
 
-                    //     if (columnPlayer >= 0 && this.matrix[rowPlayer][columnPlayer] === this.getCurrentPlayer && counter < 4) {
-                    //         counter++;
-
-                    //     }
-                    //     else {
-                    //         j = 4;
-                    //     }
-                    // }
+                        if (rowPlayer > 0 && this.matrix[rowPlayer][columnPlayer] !== this.getCurrentPlayer && counter < 4) {
+                            counter++;
+                        }
+                        else {
+                            j = 4;
+                        }
+                    }
                 }
             }
 
             if (counter === 4) {
-                alert('Victoire en horizontal de ' + this.getCurrentPlayer + ', avec ' + counter + ' points.');
+                alert('Victoire en vertical de ' + this.getCurrentPlayer + ', avec ' + counter + ' points.');
+            }
+
+            // counter = 1;
+            // rowPlayer = row;
+            // columnPlayer = column;
+
+            // for (let i = 1; i < 4; i++) {
+            //     columnPlayer++;
+            //     // rowPlayer++;
+            //     if (columnPlayer <= this.nbColumnOfP4 && rowPlayer <= nbRowOfP4) {
+
+            //         if (this.matrix[rowPlayer][columnPlayer] === this.getCurrentPlayer && counter < 4) {
+            //             counter++;
+            //         }
+            //     }
+            //     else {
+            //         i = 4;
+            //         console.log(counter, i);
+
+            // let rowPlayer = row;
+            // let columnPlayer = column;
+
+            // for (let j = 0; j < 4; j++) {
+            //     columnPlayer--;
+
+            //     if (columnPlayer >= 0 && this.matrix[rowPlayer][columnPlayer] === this.getCurrentPlayer && counter < 4) {
+            //         counter++;
+
+            //     }
+            //     else {
+            //         j = 4;
+            //     }
+            // }
+            // }
+            // }
+
+            if (counter === 4) {
+                alert('Victoire en diagonale de ' + this.getCurrentPlayer + ', avec ' + counter + ' points.');
             }
 
             // console.log('column : ' + column, 'columnPlayer : ' + columnPlayer, ' counter : ' + counter, this.matrix[row][column], this.getCurrentPlayer, ' nb : ' + this.nbColumnOfP4);
@@ -159,12 +174,12 @@ const appPuissanceQuatre = {
         play(column) {
             const currentColumn = this.matrix.map(row => row[column]);
             const row = currentColumn.lastIndexOf(null);
-            this.matrix[row][column] = this.getCurrentPlayer;
-            this.checkWinner(row, column);
-            console.log(row, column);
-            
 
-            if (column !== -1) {
+            console.log(column, row);
+
+            if (row !== -1) {
+                this.matrix[row][column] = this.getCurrentPlayer;
+                this.checkWinner(row, column);
                 this.changePlayer();
             }
         }
