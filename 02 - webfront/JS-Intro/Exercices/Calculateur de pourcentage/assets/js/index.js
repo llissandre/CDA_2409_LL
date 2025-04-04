7d561e7cd6df03e797ef88629a525b0c1fbd23ca
llissandreCRM <118750600+llissandre@users.noreply.github.com>
2025-04-04 11:50:44 +0200

    Début l'exercice calculator

---
+++ 02 - webfront/JS-Intro/Exercices/Calculateur de pourcentage/assets/js/index.js
@@ -0,0 +1,14 @@
+const appCalculator = {
+    data() {
+        return {
+
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+        
+    }
+}
+Vue.createApp(appCalculator).mount('#app');
