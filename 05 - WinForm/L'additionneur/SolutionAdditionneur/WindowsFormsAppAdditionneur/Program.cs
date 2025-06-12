fda50aaa7816cd3445b5e3053c695b1607c30432
llissandre <118750600+llissandre@users.noreply.github.com>
2025-06-12 12:40:51 +0200

    Additionneur en cours

--- 05 - WinForm/L'additionneur/SolutionAdditionneur/WindowsFormsAppAdditionneur/Program.cs
+++ 05 - WinForm/L'additionneur/SolutionAdditionneur/WindowsFormsAppAdditionneur/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsAppAdditionneur
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormAdditionneur());
         }
     }
 }
